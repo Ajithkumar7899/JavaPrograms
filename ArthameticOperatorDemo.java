40425265c3d46069f714608aaada7700cf6f1244
Ajithkumar7899 <151030210+Ajithkumar7899@users.noreply.github.com>
2023-11-17 17:54:42 +0530

    Add files via upload

---
+++ ArthameticOperatorDemo.java
@@ -0,0 +1,10 @@
+package com.edu;
+import java.util.Scanner;
+
+public class ArthameticOperatorDemo {
+public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
+System.out.println(enter the number);
+
+}
+}
